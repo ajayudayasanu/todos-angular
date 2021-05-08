6fb11b2dc8a702065e8564cc1aee2fd4bedff622
Ajay U <ajayudayasanu95@gmail.com>
2021-05-08 17:37:38 +0530

    delete tasks

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -10,8 +10,13 @@ import { Task } from '../models/Task';
 export class TaskService {
   private apiUrl = 'http://localhost:5000/tasks';
   constructor(private http: HttpClient) {}
-
+  // to get the task
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl);
   }
+  deleteTask(task:Task):Observable<Task>{
+    const url= `${this.apiUrl}/${task.id}`
+    return this.http.delete<Task>(url);
+  }
+
 }
